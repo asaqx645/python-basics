de54edbd29cece522ae97990b03c711449014372
asaqx645 <adi.sandhu@outlook.com>
2025-04-21 20:49:48 -0600

    day2.

--- day_01.py
+++ day_01.py
@@ -1,17 +1,25 @@
 # so this is a print statement and uses a string
-print("HELLO WORLD !!!")
+# print("HELLO WORLD !!!")
 # print("----------")
 # print("This is the first Print statement of programming fundamentals") 
+# print(type("68465"))
+# print(type(68465))
+
+
 # print("This is the first Print statement of programming fundamentals") 
 # print("This is the first Print statement of programming fundamentals") 
 # print("This is the first Print statement of programming fundamentals") 
 # print("----------")
 # print("string this is a string ---")
 # print(int(651561.654563456))
+# print((651561.654563456))
+# print(type(651561.654563456))
+# print("----------")
 # print("value:", int(651561.654563456))
 # print("----------")
 # print("below are floats")
-# num = 651561.654563456 
+num = 651561.654563456 
+print(num)
 # print("numtype >>> \n",type(num))
 # # num1 = 78945613.65498730000000000
 # num1 = 78945613.65498765156456456465
@@ -19,7 +27,7 @@ print("HELLO WORLD !!!")
 # print("----------")
 # print(type(int(651561.654563456)))
 # print(type(num)) # -> we figured out that it is a float
-# print(f"The value of my_float with 2 decimal places: {num:.2f}")
+print(f"The value of my_float with 2 decimal places: {num:.2f}")
 # print("The value of my_float is: %f" % num2)
 # print("The value of my_float with 1 decimal place: %.7f" % num1)
 # print("The value of my_float with 1 decimal place: %.10f" % num1) # so internally python dosent
---
+++ day_02.py
@@ -0,0 +1,15 @@
+a = 1050
+b = 2050
+c = "pizza"
+d = 2050
+
+print(type(a))
+print(type(c))
+# print()
+val = a > b
+val2 = b > a
+print("is the value of a greater than b: ", val)
+print("is the value of b greater than a: ", val2)
+noteq = b != d
+print(f"is b not equal to the value of d: {noteq}" )
+print(b == d)
